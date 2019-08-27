adc2bba988783cbf996ba8ed17c389953d950ca9
Dave Snyder <daverted@gmail.com>
2019-08-27 09:20:17 -0600

    save env in local storage

--- public/app/features/templating/template_srv.ts
+++ public/app/features/templating/template_srv.ts
@@ -310,6 +310,14 @@ export class TemplateSrv {
         }
         params['var-' + variable.name] = variable.getValueForUrl();
       }
+      // special case custom for overops - save var-environments in localStorage
+      if (variable.name === 'environments') {
+        try {
+          localStorage.setItem('overops-var-environments', variable.getValueForUrl());
+        } catch (e) {
+          console.error('unable to store environment in localStorage');
+        }
+      }
     });
 
     // save path in localStorage
